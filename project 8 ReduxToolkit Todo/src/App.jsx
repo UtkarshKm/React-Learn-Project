5744f255739ae8dfdc98a5b0be89c14bc3216945
Utkarsh Kumawat <anshukumawat77955@gmail.com>
2024-04-20 20:12:17 +0530

    Todos display , tailwind

--- project 8 ReduxToolkit Todo/src/App.jsx
+++ project 8 ReduxToolkit Todo/src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+
 
 function App() {
 
   return (
     <>
       
-      < AddTodo />
+  
+    <AddTodo/>
+    <Todo/>
         
     </>
   )
--- project 8 ReduxToolkit Todo/src/features/todo/todoSlice.js
+++ project 8 ReduxToolkit Todo/src/features/todo/todoSlice.js
@@ -1,26 +1,34 @@
-import { createSlice , isAction, nanoid } from "@reduxjs/toolkit"; 
+import {createSlice, nanoid} from "@reduxjs/toolkit";
+
 const initialState = {
-    todos:[{id: 1 , text: "hello world"}]
-}
+	todos: [{id: 1, text: "hello world"}],
+};
 
 export const todoSlice = createSlice({
-    name:"todo",
+    name: "todo",
     initialState,
-    reducers:{
-        addtodo :(state ,action)=>{    // (state , action)
-        const todo = {
-            id : nanoid(),
-            text: action.payload.text
-        } 
-        state.todos.push(todo)
-        }
-    }, 
-        deletetodo :(state , action)=>{
-            state.todos = state.todos.filter(todo => todo.id !== action.payload.id)
+    reducers: {
+        addtodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload,
+            };
+            state.todos.push(todo);
         },
-})
+        deletetodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+    },
+});
 
-export const {addtodo , deletetodo} = todoSlice.actions  // individual export of reducers functions to use in components
-export default todoSlice.reducer // export of reducer to use in store.js  
-// if you have multiple reducers then you have to export all reducers and combine them in store.js , NOw uou have only one reducer i.e todoSlice.reducer 
+export const {addtodo, deletetodo} = todoSlice.actions;
+
+// individual export of reducers functions to use in components
+
+
+
+export default todoSlice.reducer;
+
+// export of reducer to use in store.js
+// if you have multiple reducers then you have to export all reducers and combine them in store.js , NOw uou have only one reducer i.e todoSlice.reducer
 //through these reducers you can access the state of the store
